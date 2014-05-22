738cfe9721cde0e03a25e18f00772731da64df1f
Dean Gardiner <gardiner91@gmail.com>
2014-05-22 14:06:33 +1200

    [polling_xhr] fixed write bug when binary isn't supported

--- pyengineio/transports/polling_xhr.py
+++ pyengineio/transports/polling_xhr.py
@@ -5,8 +5,13 @@ class XHR_Polling(Polling):
     name = 'polling-xhr'
 
     def do_write(self, data):
+        content_type = 'application/octet-stream'
+
+        if not self.supports_binary:
+            content_type = 'text/plain; charset=UTF-8'
+
         self.poll_handle.start_response('200 OK', [
-            ('Content-Type', 'application/octet-stream'),
+            ('Content-Type', content_type),
             ('Content-Length', len(data)),
             ('Connection', 'close')
         ])
